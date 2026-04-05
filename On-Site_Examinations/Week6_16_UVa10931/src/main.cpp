a74bf480e8612717a5505e9602d1945f7806454a
Wendy <s1141409@mail.yzu.edu.tw>
2026-04-05 18:20:05 +0800

    Update main.cpp

--- On-Site_Examinations/Week6_16_UVa10931/src/main.cpp
+++ On-Site_Examinations/Week6_16_UVa10931/src/main.cpp
@@ -1,6 +1,21 @@
-#include <iostream>
+#include<iostream>
 using namespace std;
 int main(){
-  int ;
-return 0;
-}
+  int n;
+ 	 while(cin>>n&&n!=0){
+      	 int a[100],m=n,j=-1,num=0;
+     	 for(int i=0;m>0;i++){
+        	a[i]=m%2;
+        	if(m==1){
+         		 a[i+1]=1;j++;m=-1;break;}
+      			 m/=2;j++;
+     	 }
+    	cout<<"The parity of ";
+ 		 for(int i=j;i>=0;i--){
+  		    cout<<a[i];
+    		if(a[i]==1)
+     			num++;
+ 		 }
+ 		 cout<<" is "<<num<<" (mod 2)."<<endl;
+ 		 }
+    return 0;}
