13dcb485ade95d9ecd2f1a93153ad8775856d655
Wendy <s1141409@mail.yzu.edu.tw>
2026-06-08 11:38:09 +0800

    Update main.cpp

--- On-Site_Examinations/Week1_01_UVa100/src/main.cpp
+++ On-Site_Examinations/Week1_01_UVa100/src/main.cpp
@@ -1,21 +1,26 @@
 #include <iostream>
 using namespace std;
-int function(int n1,n2){
-    int max=0,Max=0;
-    for(int i=n1,i<=n2;i++){
-    if(i==1)
-        max= 1;
-    else if(i%2==1)
-        max= 1+function(3i+1);
-    else
-        max=1+function(i/2);
-    if(max>Max)
-        Max=max;
-    return Max;
-    }
+int rev(int a) {
+	if (a == 1)
+		return 1;
+	else if (a % 2)
+		return rev(3 * a + 1)+1;
+	else
+		return rev(a / 2)+1;
+}
 int main() {
-    int n1,n2;
-    while(cin>>n1>>n2)
-        cout<<n1<<n2<<function(n1,n2)<<endl;
-    return 0;
+	int n1, n2;
+	while (cin >> n1 >> n2&&n1&&n2) {
+		int max = 0;
+		if (n1 > n2) {
+			swap(n1, n2);
+			cout << n2 << " " << n1 << " ";
+		}
+		else 
+			cout << n1<< " " << n2 << " ";
+		for (int i = n1;i <= n2;i++)
+			max = rev(i) > max ? rev(i) : max;
+		cout <<max<<endl;
+	}
+	return 0;
 }
