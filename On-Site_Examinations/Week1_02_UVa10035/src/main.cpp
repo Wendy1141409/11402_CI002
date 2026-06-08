f97344b67c20e8b31e4fa91c2c7772a662f2ed98
Wendy <s1141409@mail.yzu.edu.tw>
2026-06-08 13:34:43 +0800

    Update main.cpp

--- On-Site_Examinations/Week1_02_UVa10035/src/main.cpp
+++ On-Site_Examinations/Week1_02_UVa10035/src/main.cpp
@@ -1,2 +1,22 @@
 #include <iostream>
-using namespace std ;
+using namespace std;
+int main() {
+	int n1,n2,carry,ans;
+	while (cin >> n1 >> n2 && (n1 || n2)) {
+		carry = 0;ans = 0;
+		while (n1 > 0 || n2 > 0) {
+			if (((n1 % 10) + (n2 % 10) + carry) > 9) {
+				carry = 1;ans++;
+			}
+			else
+				carry = 0;
+			n1 /= 10;n2 /= 10;
+		}
+		if (ans > 1)
+			cout << ans << " carry operations." << endl;
+		else if (ans ==1)
+			cout << ans << " carry operation." << endl;
+		else
+			cout << "No carry operation." << endl;
+	}
+}
